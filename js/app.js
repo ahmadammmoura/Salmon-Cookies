389d00f37d165152ab5ffd8835dcb95a003724b3
ahmadammmoura <ammoura575@gmail.com>
2021-03-23 16:16:45 +0200

    done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 let timeAndLocation = ['Location','6 am','7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm','8 pm','Total'];
 const dayTotal = [];
+const done = [];
 
 const tableHeader = function(){
   const table = document.getElementById('table');
@@ -22,6 +23,7 @@ const Locations = function(minNumber,maxNumber,avg,cityName,time){
   this.avg = avg;
   this.cookeisHour = [cityName];
   this.timeByHours = time;
+  dayTotal.push(this);
 };
 
 
@@ -58,7 +60,7 @@ Locations.prototype.randomNum = function (min,max){
   const Num = Math.floor(Math.random() * (max - min) + min);
   const numOfCookies = this.avg * Num;
   const randomnum=this.cookeisHour.push(Math.floor(numOfCookies));
-  dayTotal.push(Math.floor(numOfCookies));
+  // dayTotal.push(Math.floor(numOfCookies));
   return randomnum;
 };
 
@@ -73,7 +75,7 @@ Locations.prototype.total = function(){
   for(let i = 1;i < this.cookeisHour.length;i++){
     sum+=this.cookeisHour[i];
   }
-
+  done.push(sum);
   return sum;
 };
 let Seattle= new Locations(23,65,6.3,'Seattle',timeAndLocation);
@@ -92,34 +94,39 @@ Lima.renderRows();
 
 
 
+//footer total
+
+let sum = 0;
+let nums = [];
+
+for (let i = 0; i < dayTotal[0].cookeisHour.length; i++){
+  sum = dayTotal[0].cookeisHour[i] + dayTotal[1].cookeisHour[i]+dayTotal[2].cookeisHour[i] +dayTotal[3].cookeisHour[i]+dayTotal[4].cookeisHour[i];
+  nums.push(sum);
+}
+
+console.log(done);
 
 
+let sumDone =0;
 
+footer1();
+footer2();
 
-// function splitToChunks(array, parts){
-//   let result = [];
-//   for (let i = parts; i > 0; i--) {
-//     result.push(array.splice(0, Math.ceil(array.length / i)));
-//   }
-//   return result;
-// }
-// let array1 = splitToChunks(dayTotal,5)[0];
-// let array2 = splitToChunks(dayTotal,5)[1];
-// let array3 = splitToChunks(dayTotal,5)[2];
-// let array4 = splitToChunks(dayTotal,5)[3];
-// let array5 = splitToChunks(dayTotal,5)[4];
-// console.log(array1);
-
-// let sum = 0;
-// let nums = [];
-
-// for(let i = 0; i < array1.length; i++){
-//   sum = array1[i] + array2[i]+array3[i] + array4[i] + array5[i];
-//   nums.push(sum);
-// }
-
-// console.log(nums);
-
-
+function footer1(){
+  const table = document.getElementById('table');
+  const tr = document.createElement('tr');
+  table.appendChild(tr);
+  const td = document.createElement('td');
+  // tr.appendChild(td).textContent = 'Total';
+  nums[0] = 'Total';
+  for(let i = 0; i < nums.length;i++){
+    const td = document.createElement('td');
+    tr.appendChild(td).textContent = nums[i];
+  }
+  for(let i = 0; i < done.length;i++){
+    sumDone = sumDone + done[i];
+  }
+  tr.appendChild(td).textContent = sumDone;
+}
 
 
